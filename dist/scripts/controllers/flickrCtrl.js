e767de666c1efde03178282c244271d883adf1ba
fattmitch <matt.s.fitch@gmail.com>
2016-03-30 16:21:40 -0700

    photo gallery functional

--- dist/scripts/controllers/flickrCtrl.js
+++ dist/scripts/controllers/flickrCtrl.js
@@ -1,16 +1,17 @@
 (function() {
-    function flickrCtrl($scope, flickrFactory) {
+    function flickrCtrl($scope, flickrFactory, $timeout) {
         
-        
+        var imageResults = [];
         
         flickrFactory.getImagesFromUserById({
-            id:"139596112@N02",
+            id: "139596112@N02",
             lang: "en-en"
         }).then(function(_data){
             
             var images = _data.data.items;
                         
             for (var i = 0; i < images.length; i++) {
+                images[i].media.thumb = images[i].media.m.replace('_m.', '_q.');
                 images[i].media.m = images[i].media.m.replace('_m.', '_h.');
             }
                         
@@ -18,13 +19,37 @@
             
     });
         
+        $scope.currentIndex = 0;
         
-       
+        $scope.setCurrentSlideIndex = function(index) {
+            $scope.currentIndex = index;
+        };
+        
+        $scope.isCurrentSlideIndex = function(index) {
+            return $scope.currentIndex === index;
+        };
+        
+        $scope.prevSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex < $scope.imageResults.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.imageResults.length - 1;
+        };
+        
+        $scope.navHeight = function () {
+            var navHeight = document.getElementById('nav').scrollHeight;
+            console.log(navHeight);
+            
+            $scope.navPosition = {bottom: -(navHeight)+40 + "px"};   
+        }
+        
+        $timeout($scope.navHeight, 500, true);
         
         
     }
     
     angular
         .module('flickrTemplate')
-        .controller('flickrCtrl', ['$scope', 'flickrFactory', flickrCtrl]);
+        .controller('flickrCtrl', ['$scope', 'flickrFactory', '$timeout', flickrCtrl]);
 })();
